61e309cba8f206c5619925fbcf83ce0b64905bf9
Jake Heft <jakeheft@gmail.com>
2021-02-02 11:49:47 -0700

    Add new, another_method actions to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,11 +4,19 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def new
+    
+  end
+
   def create
     Item.new(params(name: "thing"))
   end
 
   def unrestful_method
     @item = Item.all
+  end
+
+  def another_method
+    @item.do_thing
   end
 end
